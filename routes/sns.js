fcdcd3e23cbe41c60097da7c8dd1a4743afb23d9
eason.liao <eason.liao@acer.com>
2015-09-10 11:34:28 +0800

    test sns

--- routes/sns.js
+++ routes/sns.js
@@ -5,18 +5,18 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log('headers: ');
-  console.log(req.headers, undefined, 2);
+  console.log(req.headers, 'undefined', 2);
   console.log('body: ');
-  console.log(req.body, undefined, 2);
+  console.log(req.body, 'undefined', 2);
   res.send('router.get respond with a resource');
 });
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   console.log('headers: ');
-  console.log(req.headers, undefined, 2);
+  console.log(req.headers, 'undefined', 2);
   console.log('body: ');
-  console.log(req.body, undefined, 2);
+  console.log(req.body, 'undefined', 2);
   res.send('router.post respond with a resource');
 });
 
